47f3a559ef26b9ec4acf564dda859bbec0c5eac9
Willian Maruyama <67539871+Dakarakoso@users.noreply.github.com>
2024-08-22 20:06:57 +0000

    added scripts lint:check and lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hedge.terraforte.agr.br
+
 A project to solve rollover managment within terra forte
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 const Home = () => {
-return <h1>Em construção</h1>
+  return <h1>Em construção</h1>;
+};
+
+function test() {
+  console.log("test");
 }
 
 export default Home;
