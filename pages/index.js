19c282728498cea83b229a7b816e0b7e0cf75d7b
Willian Maruyama <67539871+Dakarakoso@users.noreply.github.com>
2024-08-23 14:27:32 +0000

    edited index page to show under construction layout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,49 @@
 const Home = () => {
-  return <h1>Em construção</h1>;
-};
+  return (
+    <main
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100dvw",
+        height: "100dvh",
+        overflowX: "hidden",
+      }}
+    >
+      <h1 style={{ textAlign: "center" }}>Em construção 🏗️🚧</h1>
+      <span stye={{ color: "#1e3823" }}>
+        Solução para controle rollover para contratos futuros
+      </span>
+      <script
+        src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
+        type="module"
+      ></script>
 
-function test() {
-  console.log("test");
-}
+      <dotlottie-player
+        src="https://lottie.host/64b0fb5e-09f1-4110-b03f-58380b49e44d/F8HrF5vOfV.json"
+        background="transparent"
+        speed="1"
+        style={{ width: "800px", height: "500px" }}
+        loop
+        autoplay
+      ></dotlottie-player>
+
+      <footer>
+        <span>Quem somos? </span>
+        <a target="_blank" href="https://terraforte.agr.br/">
+          Terra Forte
+        </a>
+
+        <div>
+          <span>&copy; Made by </span>
+          <a target="_blank" href="https://github.com/Dakarakoso">
+            Willian Maruyama
+          </a>
+        </div>
+      </footer>
+    </main>
+  );
+};
 
 export default Home;
