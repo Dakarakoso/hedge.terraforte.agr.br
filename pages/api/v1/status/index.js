e9f73c7360d4c755bf1b0da491402c25dca50355
dakarakoso <willian_jvm@hotmail.com>
2024-08-30 15:40:26 -0300

    feat(api/status): add database connection details to /api/v1/status response
    - Enhanced the /api/v1/status endpoint to include database connection details.
    - Added `max_connections` and `opened_connections` to the response to provide information about the PostgreSQL database configuration.
    - Updated integration tests to validate the new fields in the response body, ensuring `updated_at` is a valid ISO date string and `dbVersion` is correctly set.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js";
 
 const status = async (request, response) => {
-  const result = await database.query("SELECT NOW()");
-  console.log(result.rows[0]);
-  response.status(200).json({ key: "item", titulo: "contrato de numseioque" });
+  const updatedAt = new Date().toISOString();
+  const dbVersionQuery = await database.query("SHOW server_version");
+  const dbVersion = dbVersionQuery.rows[0].server_version;
+  const dbMaxConnectionsQuery = await database.query("SHOW max_connections;");
+  const dbMaxConnections = parseInt(
+    dbMaxConnectionsQuery.rows[0].max_connections,
+  );
+  const openedConnectionsQuery = await database.query(
+    "SELECT COUNT(*) AS open_connections FROM pg_stat_activity;",
+  );
+  const openedConnections = openedConnectionsQuery.rows[0].open_connections;
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: dbMaxConnections,
+        opened_connections: openedConnections,
+      },
+    },
+  });
 };
 
 export default status;
