5af6d2267f0027adfe0e438fc17fdd3e513b1832
Willian Maruyama <67539871+Dakarakoso@users.noreply.github.com>
2024-09-02 17:56:16 +0000

    updated hard coded value to use env variables

--- infra/database.js
+++ infra/database.js
@@ -2,19 +2,19 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    user: "postgres",
-    password: "test",
-    host: "localhost",
-    port: 5432,
-    database: "postgres",
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
     ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   console.log("DB credentials: ", {
-    user: "postgres",
-    password: "test",
-    host: "localhost",
-    port: 5432,
-    database: "postgres",
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
   });
   try {
     await client.connect();
