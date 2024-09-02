e9c51aa62ba6d90f9743cdf8146df873f378d3a4
Willian Maruyama <67539871+Dakarakoso@users.noreply.github.com>
2024-09-02 17:49:43 +0000

    added ssl for DB

--- infra/database.js
+++ infra/database.js
@@ -2,16 +2,30 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    user: "postgres",
+    password: "test",
+    host: "localhost",
+    port: 5432,
+    database: "postgres",
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  console.log("DB credentials: ", {
+    user: "postgres",
+    password: "test",
+    host: "localhost",
+    port: 5432,
+    database: "postgres",
+  });
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
