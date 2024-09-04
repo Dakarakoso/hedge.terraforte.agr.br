3d11402d71dfb99aa16dac013b8e7ac075ce48c1
Willian Maruyama <67539871+Dakarakoso@users.noreply.github.com>
2024-09-04 18:47:03 +0000

    Refactored GET and POST handlers to use the default options.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,30 +2,27 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  console.log(process.env.NODE_ENV);
+  const defaultMigrationsOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
   if (request.method === "GET") {
-    console.log("==================== GET =====================");
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
-    return response.status(200).json(migrations);
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    return response.status(200).json(pendingMigrations);
   }
   if (request.method === "POST") {
-    console.log("================= POST ====================");
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     });
-    return response.status(200).json(migrations);
+    if (migratedMigrations.length > 0) {
+      return response.status(200).json(migratedMigrations);
+    }
+    return response.status(201).json(migratedMigrations);
   }
   return response.status(405).end();
 }
