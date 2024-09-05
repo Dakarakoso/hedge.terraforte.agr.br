21f1bd78158204482bd63b31b453337fc41b664b
Willian Maruyama <67539871+Dakarakoso@users.noreply.github.com>
2024-09-05 11:58:00 +0000

    ´api/v1/migrations /endpoint´

--- infra/database.js
+++ infra/database.js
@@ -1,16 +1,9 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    ssl: getSslValue(),
-  });
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -29,7 +22,19 @@ function getSslValue() {
   }
   return process.env.NODE_ENV === "production" ? true : false;
 }
-
+async function getNewClient() {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    ssl: getSslValue(),
+  });
+  await client.connect();
+  return client;
+}
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
