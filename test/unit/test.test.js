49cf9d503176ce85231925917eca0113e97cf3a7
dakarakoso <willian_jvm@hotmail.com>
2024-09-23 17:03:57 -0300

    Adds Github Actions

--- test/unit/test.test.js
+++
@@ -1,9 +0,0 @@
-const createPhrase = require("../../models/string");
-
-const string1 = "banana";
-const string2 = "apple";
-
-test("test if string = banana", () => {
-  const result = createPhrase.createPhrase(string1, string2);
-  expect(result).toBe("banana apple");
-});
